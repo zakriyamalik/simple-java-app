2682fe72dcf41cea638b64c885854eb965012aec
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-12-08 23:18:46 +0500

    HelloWorldTest

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    void testHelloWorld() {
+        assertEquals(5, 2 + 3, "Basic math test");
+    }
+}
