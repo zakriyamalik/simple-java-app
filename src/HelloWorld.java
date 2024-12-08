9e789aed34985d5f2404d2a9e8c04f715a55e837
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-12-08 15:18:51 +0500

    update again

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome to github actions!");
+        System.out.println("\nHello again\n");
 
 
     }
